006803ab11020f3415caf0be61f945389df5d616
Ayush Singhal <ayushsinghal998@gmail.com>
2021-06-19 14:34:16 +0530

    add channel model, app, serializer,utils and config settings

--- video_library/channel/models.py
+++ video_library/channel/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from video_library.core.models import BaseModel
 
-# Create your models here.
+
+class Channel(BaseModel):
+    name = models.CharField(max_length=200)
+    channel_id = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "{0}".format(str(self.id))
+
+    class Meta:
+        db_table = "channel"
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+        verbose_name = "Channel"
+        verbose_name_plural = "Channels"
