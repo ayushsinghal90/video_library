66508eddedb9cfb97542fda399da8bdb05003f2b
Ayush Singhal <ayushsinghal998@gmail.com>
2021-06-19 16:21:23 +0530

    adding sort mapping constants

---
+++ video_library/video/constants.py
@@ -0,0 +1,11 @@
+TITLE = "title"
+DESCRIPTION = "description"
+PUBLISHED_DATE = "publishing_on"
+CHANNEL_NAME = "channel"
+
+
+SORT_MAPPER = {
+    TITLE: "title",
+    DESCRIPTION: "description",
+    PUBLISHED_DATE: "publishing_on",
+}
