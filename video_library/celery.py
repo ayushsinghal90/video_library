ec2a34242099282ebbea5b9a0ad9433493cabf74
Ayush Singhal <ayushsinghal998@gmail.com>
2021-06-19 16:27:54 +0530

    celery config

--- video_library/celery.py
+++ video_library/celery.py
@@ -6,8 +6,8 @@ from celery import Celery
 from django.conf import settings
 from celery.schedules import crontab
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'youtube_videos.settings')
-app = Celery('youtube_videos')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'video_library.settings')
+app = Celery('video_library')
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
@@ -15,7 +15,7 @@ LOG = logging.getLogger(__name__)
 
 app.conf.beat_schedule = {
     'youtube_sync_engine': {
-        'task': 'youtube_videos.scheduled_task.tasks.youtube_sync_engine',
+        'task': 'video_library.scheduled_task.tasks.youtube_sync_engine',
         'schedule': crontab(minute='*/1')  # execute every minute
     }
 }
